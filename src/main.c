8542a2514f41f66fc5d6ad718e4ec85664cf1c80
Jory Stewart <jorymstewart@gmail.com>
2023-09-11 23:18:35 +0900

    Initial commit

--- src/main.c
+++ src/main.c
@@ -1,6 +1,11 @@
-#include <zephyr/kernel.h>
+#include <kernel.h>
+#include <drivers/spi.h>
 
-int main(void)
+#define SPI_MODE        SPI_OP_MODE_MASTER | SPI_WORD_SET(8) | SPI_TRANSFER_MSB
+#define I2C1_NODE       DT_NODELABEL(i2c1)
+
+
+int main()
 {
-        return 0;
+    return 0;
 }
